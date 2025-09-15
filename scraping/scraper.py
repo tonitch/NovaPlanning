0f1afa8aab8b19d62a6a7ca49f94c04974cac226
Anthony Debucquoy <tonitch@herisson.ovh>
2025-09-15 09:04:55 +0200

    cleanup

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, MoveTargetOutOfBoundsException
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, MoveTargetOutOfBoundsException, NoSuchElementException
 
-from datetime import datetime   
+from datetime import datetime
 import dateparser
 import json
 import time
@@ -15,15 +15,16 @@ import os
 import random
 import re
 
-with open('course.json') as json_file:
-    course_dict = json.load(json_file)
-
-with open('year_dict.json') as json_file:
-    year_dict = json.load(json_file)
-
-WAITING_TIME = 5
 output = {}
-list_names = []
+
+temp_filter = [
+    "Sc. informatiques",
+    "Sc. mathématiques",
+    # "Sc. chimiques",
+    "Sc. physiques",
+    # "Sc. biomédicales",
+    # "Sc. pharmaceutiques"
+    ]
 
 colorTab = {
     "BLUE": "#5c98ff",
@@ -42,59 +43,13 @@ def getColor(color):
     else:
         return "#0026ad"
 
-def move_to_start_position(driver):
-    # Go into "Formations" tab
-    formation = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[text()="Formations"]')))
-    # formation = driver.find_element(By.XPATH, '//div[text()="Formations"]')
-    action = ActionChains(driver)
-    action.move_to_element(formation)
-    action.click()
-    action.perform()
-
-    # Go into "Récapitulatif des cours" tab
-    recap = driver.find_elements(By.XPATH , '//li[@data-genre="DIPLOME.RECAPCOURS"]')[1]
-    action = ActionChains(driver)
-    action.move_to_element(recap)
-    action.click()
-    action.perform()
-
-
-def move_to_combo(driver):
-    select = driver.find_element(By.XPATH, '//div[@class="ocb_cont as-input as-select  ie-ripple"]')
-    action = ActionChains(driver)
-    action.move_to_element(select)
-    action.click()
-    action.perform()
-
-
-def move_down(driver,n,begin_enter=False):
-    time.sleep(0.3)
-    action = ActionChains(driver)
-    if(begin_enter):
-        action.send_keys(Keys.ENTER)
-    for i in range(n):
-       action.send_keys(Keys.ARROW_DOWN)
-    action.send_keys(Keys.ENTER)
-    action.perform()
-    WebDriverWait(driver, 10).until(EC.staleness_of)
-
-
-temp_filter = [
-    "Sc. informatiques",
-    "Sc. chimiques",
-    "Sc. physiques",
-    "Sc. biomédicales",
-    "Sc. pharmaceutiques"
-    ]
-
 def get_information(driver):
 
     cursus_id = driver.find_element(By.XPATH,'//div[@id="GInterface.Instances[1].Instances[1].bouton_Edit"]').text
 
     # if no course skip this cursus
-    if len(driver.find_elements(By.XPATH, '//table[@class="Table"]/tbody/tr/td/br'))>0: return True
-    
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,'//table[@class="as-content"]/tbody/tr')))
+    # if len(driver.find_elements(By.XPATH, '//table[@class="Table"]/tbody/tr/td/br'))>0: return True
+
 
     # extract year and cursus 
     res = re.search(r"\.?([\w\d.\s]+)\s(?:-|en|–|‑)\s([a-zA-ZÀ-Ÿ-.'\.\s]+)(?:,\s([^\(\)]+))?(\(\w+\))*", cursus_id) # split year, cursus, specialisation and site
@@ -103,131 +58,143 @@ def get_information(driver):
     if not res:
         print("invalid cursus :", cursus_id)
         return
-    
-    if res.group(1) not in year_dict:
-        year_dict[res.group(1)] = res.group(1)
 
-    year = year_dict[res.group(1)]
+    year = res.group(1)
     cursus = res.group(2).strip()
     specialisation = res.group(3)
     site = res.group(4)
 
-    if cursus not in temp_filter: return
+    if cursus not in temp_filter:
+        print(f"skipping {cursus}")
+        return
 
-    print(f"Cursus : {cursus}\nYear : {year}\nSpecialisation : {specialisation}\nSite : {site}")
+    print(f"Cursus : {cursus} -- Year : {year} -- Specialisation : {specialisation} -- Site : {site}")
 
-    # if cursus doesn't exist then create it
-    if(cursus not in course_dict):
-        course_dict[cursus] = {
-            "name": cursus,
-            "courses" : {}
-            }
-        
-    print(cursus, year)
-        
-    # get readable name of cursus and dict of courses
-    cursus_name = course_dict[cursus]["name"]
-    courses = course_dict[cursus]["courses"]
+    output[cursus] = {}
+    output[cursus][year] = {}
 
-    if cursus_name not in output:
-        print("cursus not found, creating")
-        output[cursus_name] = {}
+    deploy_all_courses(driver)
+    get_printable(driver)
 
-    if specialisation: year + " " + specialisation
-    if site: year = year + " " + site
-
-    print("final section :", year)
-
-    if year not in output[cursus_name]:
-        output[cursus_name][year] = {}
-
-    tables = driver.find_elements(By.XPATH,'//table[@class="as-content"]/tbody/tr')
+    table = driver.find_elements(By.XPATH,'//div[@class="Fenetre_Impression FondBlanc"]//table[@class="FondBlanc Table"]//table[@class="Table"]/tbody/tr')
     color = getColor("")
 
-
-    for i in range(1,len(tables),2):
-        course_name = tables[i].find_elements(By.XPATH,'./td/table/tbody/tr/td/span')[0].text
-
-        # ignore "_" course
-        if len(course_name) < 2 or course_name in output[cursus_name][year]:
-            continue
-
-        print(" -",course_name)
-
-        # if the course is in the list we fetch the data
-        # else we create a new entry for modification
-        if course_name in courses:
-            course_name = courses[course_name]["title"]
-            color_ = courses[course_name]["color"]
-            color = getColor(color_)
+    list_horaire = []
+    for ligne in table:
+        # print(ligne.text)
+        if "séance" in ligne.text:
+            course_name = ligne.find_element(By.XPATH, './td[2]')
+            output[cursus][year][course_name.text] = []
         else:
-            course_dict[cursus]["courses"][course_name] = {
-                "title" : course_name,
-                "color" : random.choice(list(colorTab.keys()))
-            }
+            left = ligne.find_element(By.XPATH, './td[1]')
+            spliter = re.search(r'(.*) de (\d+h\d+) à (\d+h\d+)', left.text)
+            date = spliter.group(1) if spliter else left.text 
+            start = spliter.group(2) if spliter and len(spliter.groups()) >= 2 else ""
+            end = spliter.group(3) if spliter and len(spliter.groups()) >= 3 else ""
+
+            right = ligne.find_element(By.XPATH, './td[2]')
+            spliter = re.search(r'(.*)(?: - (.*))', right.text)
+            room = spliter.group(1) if spliter else right.text
+            teacher = spliter.group(2) if spliter else ""
+
+            if start and end:
+                start = dateparser.parse(date + ' ' + start).strftime("%Y-%m-%dT%H:%M:%S")
+                end = dateparser.parse(date + ' ' + end).strftime("%Y-%m-%dT%H:%M:%S")
+
+                title = course_name.text + '\n' + teacher + '\n' + room
+                output[cursus][year][course_name.text].append(
+                        {'title':title,'start':start,'end':end, 'color': color}
+                        )
+    close_printable(driver)
+
+def select_recap_cours(driver):
+    formation = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[text()="Formations"]')))
+    action = ActionChains(driver)
+    action.move_to_element(formation)
+    action.click()
+    action.perform()
+
+    recap = driver.find_elements(By.XPATH , '//li[@data-genre="DIPLOME.RECAPCOURS"]')[1]
+    action = ActionChains(driver)
+    action.move_to_element(recap)
+    action.click()
+    action.perform()
 
 
-        if course_name not in output[cursus_name][year]:
-            output[cursus_name][year][course_name] = []
+def click_dropdown_cours(driver):
+    select = driver.find_element(By.XPATH, '//div[@class="ocb_cont as-input as-select  ie-ripple"]')
+    action = ActionChains(driver)
+    action.move_to_element(select)
+    action.click()
+    action.perform()
+    # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'liste-as-options')))
 
-        list_courses = tables[i+1].find_elements(By.XPATH,'./td/div/table/tbody/tr')
-        for course in list_courses:
-            info  = course.find_elements(By.XPATH,'./td')
-            date  = info[0].find_element(By.XPATH,'./div').get_attribute("innerHTML").replace("&nbsp;", " ")
-            _time = info[1].get_attribute("innerHTML").replace("&nbsp;", " ")
-            row_date = dateparser.parse(date + ' ' + _time[3:8])
-            if row_date != None:
-                start = row_date.strftime("%Y-%m-%dT%H:%M:%S")
-                end   = dateparser.parse(date + ' ' + _time[10:17]).strftime("%Y-%m-%dT%H:%M:%S")
 
-                teacher = info[3].get_attribute("innerHTML").replace("&nbsp;", " ")
-                room = info[4].get_attribute("innerHTML").replace("&nbsp;", " ")
-                title = course_name + '\n' + teacher + '\n' + room
+def move_down(driver,n, i):
+    el = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="GInterface.Instances[1].Instances[1]_{i}"]')))
+    action = ActionChains(driver)
+    for i in range(n):
+       action.send_keys(Keys.ARROW_DOWN)
+    action.send_keys(Keys.ENTER)
+    action.perform()
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element(el))
 
-                output[cursus_name][year][course_name].append({'title':title,'start':start,'end':end, 'color': color})
+
+def deploy_all_courses(driver):
+
+    action = ActionChains(driver)
+    for i in range(5):
+        try:
+            first = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.liste_contenu_ligne')))
+            action.send_keys_to_element(first, Keys.END)
+            action.send_keys(Keys.TAB)
+            action.send_keys(Keys.ENTER)
+            action.perform()
+        except StaleElementReferenceException:
+            continue
+        finally:
+            break
+
+    action = ActionChains(driver)
+    while len(driver.find_elements(By.CSS_SELECTOR, '.btn-deploiement.icon_fleche_num')) > 0:
+        action.send_keys(Keys.UP)
+        action.send_keys(Keys.TAB)
+        action.send_keys(Keys.ENTER)
+        action.perform()
+
+def get_printable(driver):
+    print_icon = driver.find_element(By.CLASS_NAME, 'icon_print')
+    action = ActionChains(driver)
+    action.move_to_element(print_icon)
+    action.click()
+    action.perform()
+
+def close_printable(driver):
+    close_icon = driver.find_element(By.CLASS_NAME, 'icon_fermeture_widget')
+    action = ActionChains(driver)
+    action.move_to_element(close_icon)
+    action.click()
+    action.perform()
 
 
 options = Options()
-options.add_argument('-headless')
+# options.add_argument('-headless')
 driver = webdriver.Firefox(options=options)
-url = "https://hplanning2024.umons.ac.be/invite"
+driver.set_window_size(1920, 1080)
+url = "https://hplanning2025.umons.ac.be/invite"
 driver.get(url)
 driver.refresh()
-end = True
-for i in range(248):
 
-    success = False
-    while not success:
-        try:
-            move_to_start_position(driver)
-            move_to_combo(driver)
-            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'liste-as-options')))
-            move_down(driver,i)
-            get_information(driver)
+select_recap_cours(driver)
 
-            with open('course.json', 'w') as my_file:
-                my_file.writelines(json.dumps(course_dict, indent=4, ensure_ascii=False))
 
-            with open('year_dict.json', 'w') as my_file:
-                my_file.writelines(json.dumps(year_dict, indent=4, ensure_ascii=False))
-
-            with open('events.json', 'w') as my_file:
-                my_file.writelines(json.dumps(output, indent=4, ensure_ascii=False))
-
-            success = True
-
-            print(f"{int(i/248*100)}% [{int(i/248*20) * '#' + int(20 - i/248*20) * ' '}]")
-
-            driver.refresh()
-        except Exception as e:
-            print("error", e)
-            driver.close()
-            driver = webdriver.Firefox(options)
-            driver.get(url)
-            driver.refresh()
-
-    driver.implicitly_wait(1)
-    
+click_dropdown_cours(driver)
+move_down(driver,0, 2)
+get_information(driver)
+for i in range(262):
+    click_dropdown_cours(driver)
+    move_down(driver,1, i + 2)
+    get_information(driver)
 
 driver.close()
 
--- script/main.js
+++ script/main.js
@@ -2,6 +2,5 @@ const ics_events = [];
 
 async function fetch_events(){
 
-    return  fetch('https://raw.githubusercontent.com/Shinkumons/NovaPlanning/action/events.json')
-        .then((response) => response.json())
+    return  fetch('events.json').then((response) => response.json())
 }
