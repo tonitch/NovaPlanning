541c719a70e445ec6d5436ffb13440576c3ea20b
Anthony Debucquoy <debucquoy.anthony@gmail.com>
2025-09-26 20:02:27 +0200

    courses from action branch

--- script/main.js
+++ script/main.js
@@ -2,5 +2,6 @@ const ics_events = [];
 
 async function fetch_events(){
 
-    return  fetch('events.json').then((response) => response.json())
+    // return  fetch('events.json').then((response) => response.json())
+    return  fetch('https://raw.githubusercontent.com/tonitch/NovaPlanning/refs/heads/action/events.json').then((response) => response.json())
 }
